a29845b42ea5c2598287aaee46523492be9821a0
lemonchen <ayaboy1124@126.com>
2018-12-10 17:24:45 +0800

    部分修改

--- countview/src/main/java/com/cx/countview/CountView.java
+++ countview/src/main/java/com/cx/countview/CountView.java
@@ -3,8 +3,12 @@ package com.cx.countview;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.text.method.DigitsKeyListener;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -29,13 +33,15 @@ public class CountView extends LinearLayout {
         @Override
         public void onClick(View v) {
             if(count + 1 > MAX_COUNT){
-                Toast.makeText(getContext(),maxNotice, Toast.LENGTH_SHORT);
+                if(!TextUtils.isEmpty(maxNotice))
+                    showToast(maxNotice);
             }else if(count == MIN_COUNT){
                 showView();
                 setCount(count + 1);
             }else {
                 setCount(count + 1);
             }
+            clearEditFocus();
         }
     };
 
@@ -45,9 +51,12 @@ public class CountView extends LinearLayout {
             if(count - 1 <= MIN_COUNT){
                 hideView();
                 setCount(MIN_COUNT);
+                if(!TextUtils.isEmpty(minNotice))
+                    showToast(minNotice);
             }else {
                 setCount(count - 1);
             }
+            clearEditFocus();
         }
     };
 
@@ -92,6 +101,92 @@ public class CountView extends LinearLayout {
         maxNotice = arr.getString(R.styleable.CountView_max_notice_str);
         minNotice = arr.getString(R.styleable.CountView_min_notice_str);
         etCount.setKeyListener(DigitsKeyListener.getInstance("0123456789"));
+        etCount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String str = s.toString();
+                if(TextUtils.isEmpty(str)){
+                    etCount.setText(MIN_COUNT+"");
+                    etCount.setSelection(etCount.getText().toString().length());
+                    if(!TextUtils.isEmpty(minNotice)){
+                        showToast(minNotice);
+                    }
+                }else if(str.length() >= 2 && str.startsWith("0")){
+                    etCount.setText(Integer.parseInt(str)+"");
+                    etCount.setSelection(etCount.getText().toString().length());
+                }else if(Integer.parseInt(str) > MAX_COUNT){
+                    etCount.setText(MAX_COUNT+"");
+                    etCount.setSelection(etCount.getText().toString().length());
+                    if(!TextUtils.isEmpty(maxNotice)){
+                        showToast(maxNotice);
+                    }
+                }else if(Integer.parseInt(str) < MIN_COUNT){
+                    etCount.setText(MIN_COUNT+"");
+                    etCount.setSelection(etCount.getText().toString().length());
+                    if(!TextUtils.isEmpty(minNotice)){
+                        showToast(minNotice);
+                    }
+                }
+                count = Integer.parseInt(etCount.getText().toString());
+            }
+        });
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    }
+
+    private int measureWidth(int measureSpec) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+        if (specMode == MeasureSpec.EXACTLY) {
+            // We were told how big to be
+            result = specSize;
+        } else {
+            // Measure the text
+
+            if (specMode == MeasureSpec.AT_MOST) {
+                // Respect AT_MOST value if that was what is called for by measureSpec
+                result = Math.min(result, specSize);
+            }
+        }
+        return result;
+    }
+
+    private int measureHeight(int measureSpec) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.EXACTLY) {
+            // We were told how big to be
+            result = specSize;
+        } else {
+            // Measure the text (beware: ascent is a negative number)
+
+            if (specMode == MeasureSpec.AT_MOST) {
+                // Respect AT_MOST value if that was what is called for by measureSpec
+                result = Math.min(result, specSize);
+            }
+        }
+        return result;
+    }
+
+    private void showToast(String str){
+        Toast.makeText(CountView.this.getContext(),str,Toast.LENGTH_SHORT).show();
     }
 
     public void setMinCount(int min){
@@ -110,11 +205,11 @@ public class CountView extends LinearLayout {
         this.MAX_COUNT = max;
     }
 
-    private void showView(){
+    public void showView(){
 
     }
 
-    private void hideView(){
+    public void hideView(){
 
     }
 
@@ -123,7 +218,13 @@ public class CountView extends LinearLayout {
         etCount.setFocusable(true);
         etCount.setFocusableInTouchMode(true);
         etCount.requestFocus();
+        etCount.setSelection(etCount.length());
         InputMethodManager input = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
         input.showSoftInput(etCount, InputMethodManager.SHOW_FORCED);
+    }
+
+    private void clearEditFocus(){
+        etCount.clearFocus();
+        this.requestFocus();
     }
 }
